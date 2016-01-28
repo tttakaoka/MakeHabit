46376d3d2483c2adc82d711f4cf2308ffd31ff9a
Takaoka Kazumasa <takaoka.kazumasa0510@gmail.com>
2016-01-28 23:05:04 +0900

    success test for parse

--- app/src/main/java/tttakaoka/makehabit/MakeHabitApplication.java
+++ app/src/main/java/tttakaoka/makehabit/MakeHabitApplication.java
@@ -2,8 +2,26 @@ package tttakaoka.makehabit;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
 /**
  * Application class
  */
 public class MakeHabitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
 }
