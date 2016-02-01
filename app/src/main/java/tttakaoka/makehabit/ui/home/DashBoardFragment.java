343a8f4c06bc40c0a728d63297b1f47aa2469b48
Takaoka Kazumasa <takaoka.kazumasa0510@gmail.com>
2016-02-01 18:35:09 +0900

    add dash board fragment constructor

--- app/src/main/java/tttakaoka/makehabit/ui/home/DashBoardFragment.java
+++ app/src/main/java/tttakaoka/makehabit/ui/home/DashBoardFragment.java
@@ -1,10 +1,30 @@
 package tttakaoka.makehabit.ui.home;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tttakaoka.makehabit.R;
 
 /**
- * Created by takaoka on 2016/02/01.
+ * ダッシュボード
  */
 public class DashBoardFragment extends Fragment {
 
+    public DashBoardFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dash_board, container, false);
+    }
 }
